a641f99a472aec3a1dd316c3ade51fe4a4425120
Dmitry <125144429+iMa3X@users.noreply.github.com>
2023-02-26 11:57:58 +0300

    Some changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello i'm new Calculator!");
+
         Reader read = new Reader();
         Calcalator calc = new Calcalator();
 
